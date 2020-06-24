bd8f9e86dbfd578d4443a6b3b85df08ff5982da0
alexandresantosm <amenezes031@gmail.com>
2020-06-23 23:44:07 -0300

    Populando o banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO contatos (id, nome, telefone, email) VALUES
+(1, 'Maria', '12345678', 'maria@email.com'),
+(2, 'João', '00000000', 'joao@email.com'),
+(3, 'Pedro', '11111111', 'pedro@email.com'),
+(4, 'Fátima', '22222222', 'fatima@email.com'),
+(5, 'Daniela', '33333333', 'daniela@email.com'),
+(6, 'Henrique', '44444444', 'henrique@email.com'),
+(7, 'Francisco', '55555555', 'francisco@email.com'),
+(8, 'Lucas', '66666666', 'lucas@email.com'),
+(9, 'Rafael', '77777777', 'rafael@email.com'),
+(10, 'Zilda', '88888888', 'zilda@email.com');
